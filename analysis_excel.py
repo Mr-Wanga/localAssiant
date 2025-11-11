af323e41fac5fb8ee08423f2995063d6c1573f21
Mr-Wanga <949278522@qq.com>
2025-11-11 14:21:05 +0000

    update call mcp func

--- analysis_excel.py
+++ analysis_excel.py
@@ -1,27 +1,62 @@
-import requests
-import json
+import httpx
+
 
 url = "http://192.168.31.40:9000/mcp"
 
-headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json, text/event-stream",
-    "X-Session-ID": "test-session"
-}
 
+async def get_mcp_session_id(url: str):
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json, text/event-stream"
+    }
 
-payload = {
-    "jsonrpc": "2.0",
-    "method": "tools/call",
-    "params": {
-        "session": "test-session",
-        "name": "detect_excel_tables",
-        "arguments": {
-            "excel_name": "example.xlsx"
+    payload = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+            "protocolVersion": "2024-11-05",
+            "capabilities": {
+                "roots": {},
+                "sampling": {}
+            },
+            "clientInfo": {
+                "name": "init-server",
+                "version": "1.0.0"
+            }
         }
-    },
-    "id": "1"
-}
+    }
 
-response = requests.post(url, json=payload, headers=headers)
-print(json.dumps(response.json(), ensure_ascii=False, indent=2))
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, json=payload, headers=headers)
+        response.raise_for_status()
+
+        session_id = response.headers['mcp-session-id']
+    
+    return session_id
+
+async def get_mcp_result(url: str, session_id: str, tool_name: str, tool_params: dict):
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json, text/event-stream",
+        "mcp-session-id": session_id
+    }
+
+    payload = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "tools/call",
+        "params": {
+            "name": tool_name,
+            "arguments": tool_params
+        }
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, json=payload, headers=headers)
+        response.raise_for_status()
+        
+        result = response.text
+    
+    return result
+
--- mcps/excel-mcp-server/src/utils.py
+++ mcps/excel-mcp-server/src/utils.py
@@ -24,6 +24,6 @@ def find_candidate_blocks(excel_path, min_cols=3):
     tables = []
     for start, end in blocks:
         sub_df = df.loc[start:end].dropna(how="all", axis=1)
-        tables.append({"start": start, "end": end, "data": sub_df})
+        tables.append({"start": start, "end": end, "data": sub_df.to_json()})
     
     return tables
